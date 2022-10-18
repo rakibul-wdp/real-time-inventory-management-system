79405e0e9a0eb952185c3d9be73e76d3c82c9b6b
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-19 05:45:00 +0600

    setup index.js file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,41 @@
-console.log("Hello world")
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+
+const productRoutes = require("./routes/productRoutes");
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*"
+  }
+});
+
+
+app.use(express.json());
+app.use(cors())
+app.use((req, res, next) => {
+  req.io = io;
+  next()
+})
+
+
+app.use("/inventory", productRoutes);
+app.get('/', async (req, res) => {
+  res.status(200).send("Welcome to Inventory Management System!");
+});
+
+
+connectDB();
+
+httpServer.listen(8080, () => {
+  console.log("listening on port 8080");
+})
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log(socket.id, 'is disconnected.')
+  })
+})
